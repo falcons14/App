d94040c0c2fe7adde47d8812a6d5951e6094843f
mario031 <reimariodayo@gmail.com>
2015-01-15 01:18:36 +0900

    がんばったよ

---
+++ www/js/dark.js
@@ -0,0 +1,15 @@
+var THREASHOLD = 50;
+var CLASSNAME = "night";
+
+var elm = document.querySelector("h2");
+
+var update = function(event){
+  console.log(event.value);
+  if(event != null && event.value != null && event.value < THREASHOLD){
+    elm.classList.add(CLASSNAME);
+  }else{
+    elm.classList.remove(CLASSNAME);
+  }
+};
+
+window.addEventListener("devicelight", update);s="navbar navbar-default" role="navigation">
